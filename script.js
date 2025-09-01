edcda8508114a8034c17d15e0b29d613c78cb171
Jayvina Dhedhi <140063178+jayvina@users.noreply.github.com>
2025-09-01 21:55:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const mobileMenu = document.getElementById('mobile-menu');
 const navLinks = document.querySelector('.nav-links');
+const links = navLinks.querySelectorAll('a');
 
 mobileMenu.addEventListener('click', () =>{
     navLinks.classList.toggle('active');
+})
+
+links.forEach(link => {
+    link.addEventListener('click', () =>{
+        navLinks.classList.remove('active')
+    })
 })
